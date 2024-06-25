5dcbd0d7b01b1d768ea40f80e89878eebb9cb999
EsebaCarallo <kobydarkbomber@gmail.com>
2024-06-25 17:48:15 +0200

    Practica dowhile

--- src/Estructura_repetitiva_do_while.kt
+++ src/Estructura_repetitiva_do_while.kt
@@ -33,7 +33,7 @@ fun main() {
 
  */
 
-
+/*
     var cant_inferior = 0 //por debajo de 9.8
     var cant_superior = 0 //por debajo de 10.2
     var cant_adecuada = 0 //entre 9.8 y 10.2
@@ -56,4 +56,58 @@ fun main() {
     println("Cantidad total de piezas procesadas: $suma")
 
 
+ */
+
+
+    //Practicas
+/*
+    var valorAcum = 0
+    var entrada: Int
+    println("Inserte una cantidad que desea sumar")
+    println("Ingrese 9999 para finalizar")
+
+    do {
+        print("Ingrese un valor:")
+        entrada = readln().toInt()
+
+        if (entrada != 9999) {
+            valorAcum += entrada
+        }
+    }while (entrada != 9999)
+    println("El valor acumulado es: $valorAcum")
+
+    if (valorAcum == 0) {
+        println("El valor acumulado es cero")
+    (valorAcum > 0)
+    println("El valor acumulado es mayor a cero")
+}else {
+        println("El valor acumulado es menor a cero")
+    }
+
+ */
+
+    var saldos = 0.0
+
+    println("Ingrese los datos de las cuentas corrientes. Ingrese un número de cuenta negativo para finalizar.")
+
+    do {
+        print("Ingrese el número de cuenta: ")
+        val cuenta = readln().toInt()
+        if (cuenta >= 0){
+            print("Ingrese el saldo actual: ")
+            val saldo = readln().toDouble()
+            
+            if (saldo > 0.0) {
+                println("Estado de la cuenta: Acreedor")
+                saldos += saldo
+            }else
+            if (saldo < 0.0) {
+                    println("Estado de la cuenta: Deudor")
+            }else
+           if (saldo == 0.0)
+                        println("Estado de la cuenta: Nulo")}
+            }while (cuenta >= 0)
+            println("La suma total de los saldos acreedores es: $saldos")
 }
+
+

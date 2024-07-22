043d04efa8603f27fee6b4c8efb397c1acda96e9
EsebaCarallo <kobydarkbomber@gmail.com>
2024-07-22 21:18:29 +0200

    Parametros

--- src/Funciones_Parametros/Proyecto 76.kt
+++ src/Funciones_Parametros/Proyecto 76.kt
@@ -1,1 +1,20 @@
 package Funciones_Parametros
+fun mostrarPerimetro(lado:Int){
+    val perimetro = lado * 4
+    println("El perimetro es: $perimetro")
+}
+fun mostrarSuperficie(lado: Int){
+    val superficie = lado * lado
+    println("La superficie es: $superficie")
+}
+
+fun main() {
+    print("Ingrese el valor del lado de un cuadro: ")
+    val la = readln().toInt()
+    print("Quiere calcular el perimetro o la superficie[Ingresar texto:perimetro/superficie]")
+    var respuesta = readln()
+    when (respuesta){
+        "perimetro" -> mostrarPerimetro(la)
+        "superficie" -> mostrarSuperficie(la)
+    }
+}

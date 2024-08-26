16e0899fcd6196a332889d14093893e23fe145e2
EsebaCarallo <kobydarkbomber@gmail.com>
2024-08-27 00:00:51 +0200

    SE ME OLVIDAAA

---
+++ src/Colaboracion_de_clases/Practicas.kt
@@ -0,0 +1,30 @@
+package Colaboracion_de_clases
+
+class Socio(val nombre: String, val antiguedad: Int)pzdf
+class Club(val socio1: Socio, val socio2: Socio, val socio3: Socio) {
+
+
+    fun obtenerSocioConMayorAntiguedad(): Socio {
+        var mayorAntiguedad = socio1
+
+        if (socio2.antiguedad > mayorAntiguedad.antiguedad) {
+            mayorAntiguedad = socio2
+        }
+
+        if (socio3.antiguedad > mayorAntiguedad.antiguedad) {
+            mayorAntiguedad = socio3
+        }
+
+        return mayorAntiguedad
+    }
+}
+
+// Ejemplo de uso
+fun main() {
+    val socio1 = Socio("Juan", 5)
+    val socio2 = Socio("Ana", 10)
+    val socio3 = Socio("Luis", 7)
+    val club = Club(socio1, socio2, socio3)
+    val socioConMayorAntiguedad = club.obtenerSocioConMayorAntiguedad()
+    println("El socio con mayor antigüedad es: ${socioConMayorAntiguedad.nombre}")
+}

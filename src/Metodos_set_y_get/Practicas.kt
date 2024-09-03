c6197c48bf81f58b9a124c03f0c0eddaeb44232e
EsebaCarallo <kobydarkbomber@gmail.com>
2024-09-03 18:56:42 +0200

    Matrices

---
+++ src/Metodos_set_y_get/Practicas.kt
@@ -0,0 +1,53 @@
+package Metodos_set_y_get
+
+/*
+class Personaa(var nombre: String, sueldo: Int){
+    var sueldo: Int = 0
+        set(valor) {
+            if (valor >= 0)
+                field = valor
+            else
+                field = 0
+        }
+    init {
+        this.sueldo = sueldo
+    }
+    fun imprimir(){
+        println("Nombre del empleado: $nombre")
+        println("El sueldo del empleado: $sueldo ")
+    }
+}
+fun main() {
+    val persona1 = Personaa("sebas",10000)
+    persona1.imprimir()
+}
+
+ */
+
+/*
+class Dado(valor: Int){
+    var valor: Int = 1
+        set(valor){
+            if (valor <= 6)
+                field = valor
+            else
+                field = 1
+        }
+    init {
+        this.valor = valor
+    }
+    fun tirar(){
+        valor = ((Math.random()*6)+1).toInt()
+        imprimir()
+    }
+    fun imprimir(){
+        println("El numero es $valor")
+    }
+}
+fun main(){
+    val dado = Dado(7)
+    dado.imprimir()
+    dado.tirar()
+}
+
+ */
